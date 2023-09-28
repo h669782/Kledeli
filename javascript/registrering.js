85aedf66232482519275a682a49f4b8d47436b3f
Anthony - h669782 <anthony.leinebo@gmail.com>
2023-09-29 01:00:39 +0200

    Registration form
    Started work on registration form.

--- javascript/registrering.js
+++ javascript/registrering.js
@@ -7,33 +7,66 @@ document.addEventListener('DOMContentLoaded', function() {
     const containerElement = document.getElementById('container');
     const tittelElement = document.getElementById('tjenesteTittel');
 
+    // Hvis ingen gyldig tjeneste er valgt fra URL, vis pakkevalget
+    document.getElementById('formStep1').classList.remove('hidden');
+
     if(tjeneste && gyldigeTjenester.includes(tjeneste)){
-        
         tittelElement.textContent = 'Du har valgt ' + tjeneste;
-    } else {
-        if(containerElement){
-            containerElement.innerHTML = '<p>Vennligst velg en gyldig tjeneste:</p>'
+        // Velger den tilsvarende radioknappen for tjenesten fra URL
+        document.querySelector(`input[name="packageType"][value="${tjeneste}"]`).checked = true;
+    } 
+
+    document.getElementById('next1').addEventListener('click', function() {
+        const packageType = document.querySelector('input[name="packageType"]:checked').value;
         
-
-            gyldigeTjenester.forEach(function(tjenesteNavn){
-                const btn = document.createElement('button');
-                btn.textContent = tjenesteNavn;
-
-                btn.style.padding = '10px 15px';
-                btn.style.backgroundColor = '#053742';
-                btn.style.color = '#f2f5f7';
-                btn.style.border = 'none';
-                btn.style.borderRadius = '5px';
-                btn.style.cursor = 'pointer';
-                btn.style.margin = '5px';
-
-                btn.addEventListener('click', function (){
-                    window.location.href = 'registrering.html?tjeneste=' + tjenesteNavn;
-                })
-                containerElement.appendChild(btn);
-            });
-        } else {
-            console.error('ID for container blei ikkje funnet')
+        if(gyldigeTjenester.includes(packageType)){
+            tittelElement.textContent = 'Du har valgt ' + packageType;
         }
-    }
-})
+
+        document.getElementById('formStep1').classList.add('hidden');
+        document.getElementById('formStep2').classList.remove('hidden');
+        document.getElementById('step1').classList.remove('active-step');
+        document.getElementById('step2').classList.add('active-step');
+    });
+});
+
+
+
+document.getElementById('next2').addEventListener('click', function() {
+    document.getElementById('formStep2').classList.add('hidden');
+    document.getElementById('formStep3').classList.remove('hidden');
+    document.getElementById('step2').classList.remove('active-step');
+    document.getElementById('step3').classList.add('active-step');
+});
+
+document.getElementById('next3').addEventListener('click', function() {
+    document.getElementById('formStep3').classList.add('hidden');
+    document.getElementById('formStep4').classList.remove('hidden');
+    document.getElementById('step3').classList.remove('active-step');
+    document.getElementById('step4').classList.add('active-step');
+});
+
+document.getElementById('prev1').addEventListener('click', function() {
+    document.getElementById('formStep2').classList.add('hidden');
+    document.getElementById('formStep1').classList.remove('hidden');
+    document.getElementById('step2').classList.remove('active-step');
+    document.getElementById('step1').classList.add('active-step');
+});
+
+document.getElementById('prev2').addEventListener('click', function() {
+    document.getElementById('formStep3').classList.add('hidden');
+    document.getElementById('formStep2').classList.remove('hidden');
+    document.getElementById('step3').classList.remove('active-step');
+    document.getElementById('step2').classList.add('active-step');
+});
+
+document.getElementById('prev3').addEventListener('click', function() {
+    document.getElementById('formStep4').classList.add('hidden');
+    document.getElementById('formStep3').classList.remove('hidden');
+    document.getElementById('step4').classList.remove('active-step');
+    document.getElementById('step3').classList.add('active-step');
+});
+
+document.getElementById('submit').addEventListener('click', function() {
+    // Håndter innsending av skjemaet her
+});
